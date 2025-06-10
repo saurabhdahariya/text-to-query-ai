56c8bdbe08925c124d20c5f96db4b7f3a452726b
Saurabh Dahariya <saurabhdahariya81@outlook.com>
2025-06-11 02:27:01 +0530

    fixing issue

--- server/server.js
+++ server/server.js
@@ -89,6 +89,17 @@ app.use('*', (req, res) => {
   });
 });
 
+//adding for render
+const path = require('path');
+
+// Serve static files from React
+app.use(express.static(path.join(__dirname, '../build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../build/index.html'));
+});
+
+
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
   console.log(`📊 Environment: ${process.env.NODE_ENV || 'development'}`);
